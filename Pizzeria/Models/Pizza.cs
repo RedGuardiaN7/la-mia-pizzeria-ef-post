5d031866b6d93e914414d50ae63585abaf88daf5
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-12 15:33:19 +0100

    Aggiunte Data Annotaions al modello/form

--- Pizzeria/Models/Pizza.cs
+++ Pizzeria/Models/Pizza.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pizzeria.Models
 {
@@ -6,10 +8,15 @@ namespace Pizzeria.Models
     {
         public int Id { get; set; }
         [Column(TypeName = "varchar(150)")]
+        [Required(ErrorMessage = "Il nome della pizza è obbligatorio!")]
+        [StringLength(50, ErrorMessage = "Il nome della pizza è troppo lungo! (>50 caratteri)")]
         public string Name { get; set; }
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "La descrizione della pizza è obbligatoria!")]
+        [StringLength(300, ErrorMessage = "La descrizione della pizza è troppo lunga! (>300 caratteri)")]
         public string Description { get; set; }
         [Column(TypeName = "varchar(300)")]
+        [Url(ErrorMessage = "L'url inserito è invalido!")]
         public string Image { get; set; }
 
         public Pizza()
